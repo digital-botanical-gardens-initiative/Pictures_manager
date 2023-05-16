4b27c6e468ec99030749675b1758e7e721ea99a5
edouardbruelhart <edouard.bruelhart@unifr.ch>
2023-05-16 22:36:48 +0200

    change code

--- DBGI_Nexcloud_import.py
+++ DBGI_Nexcloud_import.py
@@ -8,31 +8,21 @@ import shutil
 load_dotenv()
 
 #Access the environment variables
-local_path = os.getenv('out_path')
-remote_path = os.getenv('nextcloud_path')
-nextcloud_data_folder = os.getenv('next_folder_path')
+local_path = os.getenv('out_jpg_path')
+remote_pic_path = os.getenv('nextcloud_path_pic')
+remote_csv_path = os.getenv('nextcloud_path_pic')
 
-def upload_files_recursive(local_path, remote_path):
-    for item in os.listdir(local_path):
-        item_path = os.path.join(local_path, item)
-        remote_item_path = os.path.join(remote_path, item)
+# Iterate over the directory tree rooted at the input folder
+for root, dirs, files in os.walk(local_path):
+    # Create the corresponding output directory structure
+    rel_path = os.path.relpath(root, local_path)
+    output_dir = os.path.join(remote_pic_path, rel_path)
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Move files from the input directory to the corresponding output directory
+    for file in files:
+        input_file = os.path.join(root, file)
+        output_file = os.path.join(output_dir, file)
+        shutil.move(input_file, output_file)
 
-        if os.path.isfile(item_path):
-            # Move file to Nextcloud data folder and overwrite if it already exists
-            remote_file_path = os.path.join(nextcloud_data_folder, remote_item_path[len(local_path) + 1:])
-            remote_file_dir = os.path.dirname(remote_file_path)
-            os.makedirs(remote_file_dir, exist_ok=True)
-            shutil.move(item_path, remote_file_path)
-            print(f"Moved file: {item_path} -> {remote_file_path}")
-        elif os.path.isdir(item_path):
-            # Check if directory exists in Nextcloud data folder
-            remote_directory_path = os.path.join(nextcloud_data_folder, remote_item_path[len(local_path) + 1:])
-            if not os.path.exists(remote_directory_path):
-                # Create directory in Nextcloud data folder if it doesn't exist
-                os.makedirs(remote_directory_path)
-                print(f"Created remote directory: {remote_item_path}")
-
-            # Recursively move files within the directory
-            upload_files_recursive(item_path, remote_item_path)
-
-upload_files_recursive(local_path=local_path, remote_path=remote_path)
+print("Files moved successfully.")
