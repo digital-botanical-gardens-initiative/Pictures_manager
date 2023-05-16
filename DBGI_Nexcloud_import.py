a360f5776c989a7da654d0707061e514cf424487
edouardbruelhart <edouard.bruelhart@unifr.ch>
2023-05-16 21:39:17 +0200

    changed upload method

--- DBGI_Nexcloud_import.py
+++ DBGI_Nexcloud_import.py
@@ -2,18 +2,35 @@
 
 from dotenv import load_dotenv
 import os
-from nextcloud import NextCloud
+import shutil
 
 #Loads environment variables
 load_dotenv()
 
 #Access the environment variables
-nextcloud_url = os.getenv('Instance_nextcloud')
-username = os.getenv('next_user')
-password = os.getenv('next_pass')
-login_endpoint = nextcloud_url + "/ocs/v1.php/cloud/user?format=json"
-local_folder_path = os.getenv('out_jpg_path')
+local_path = os.getenv('out_jpg_path')
+remote_path = os.getenv('nextcloud_path')
+nextcloud_data_folder = os.getenv('next_folder_path')
 
-nxc = NextCloud(endpoint=nextcloud_url, user=username, password=password)
+def upload_files_recursive(local_path, remote_path):
+    for item in os.listdir(local_path):
+        item_path = os.path.join(local_path, item)
+        remote_item_path = os.path.join(remote_path, item)
 
-nxc.get_users()
+        if os.path.isfile(item_path):
+            # Move file to Nextcloud data folder and overwrite if it already exists
+            remote_file_path = os.path.join(nextcloud_data_folder, remote_item_path.lstrip('/'))
+            shutil.move(item_path, remote_file_path)
+            print(f"Moved file: {item_path} -> {remote_file_path}")
+        elif os.path.isdir(item_path):
+            # Check if directory exists in Nextcloud data folder
+            remote_directory_path = os.path.join(nextcloud_data_folder, remote_item_path.lstrip('/'))
+            if not os.path.exists(remote_directory_path):
+                # Create directory in Nextcloud data folder if it doesn't exist
+                os.makedirs(remote_directory_path)
+                print(f"Created remote directory: {remote_item_path}")
+
+            # Recursively move files within the directory
+            upload_files_recursive(item_path, remote_item_path)
+
+upload_files_recursive(local_path=local_path, remote_path=remote_path)
