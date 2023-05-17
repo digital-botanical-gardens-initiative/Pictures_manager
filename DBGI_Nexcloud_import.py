291b37ed630989211616810f081202637c3482f8
edouardbruelhart <edouard.bruelhart@unifr.ch>
2023-05-17 09:15:51 +0200

    add csv import

--- DBGI_Nexcloud_import.py
+++ DBGI_Nexcloud_import.py
@@ -9,6 +9,7 @@ load_dotenv()
 
 #Access the environment variables
 local_path = os.getenv('out_jpg_path')
+local_path_csv = os.getenv('out_csv_path')
 remote_pic_path = os.getenv('nextcloud_path_pic')
 remote_csv_path = os.getenv('nextcloud_path_pic')
 
@@ -17,6 +18,21 @@ for root, dirs, files in os.walk(local_path):
     # Create the corresponding output directory structure
     rel_path = os.path.relpath(root, local_path)
     output_dir = os.path.join(remote_pic_path, rel_path)
+    os.makedirs(output_dir, exist_ok=True)
+    
+    # Move files from the input directory to the corresponding output directory
+    for file in files:
+        input_file = os.path.join(root, file)
+        output_file = os.path.join(output_dir, file)
+        shutil.move(input_file, output_file)
+
+print("Files moved successfully.")
+
+# Iterate over the directory tree rooted at the input folder
+for root, dirs, files in os.walk(local_path_csv):
+    # Create the corresponding output directory structure
+    rel_path = os.path.relpath(root, local_path_csv)
+    output_dir = os.path.join(remote_csv_path, rel_path)
     os.makedirs(output_dir, exist_ok=True)
     
     # Move files from the input directory to the corresponding output directory
