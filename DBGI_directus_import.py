51cf003b696756edff0c7609ae233e1948d90aa4
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-14 13:29:30 +0100

    working on directus import

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -1,103 +1,54 @@
-# import packages
-import psycopg2
+from pyinaturalist import get_observations
 import os
 from dotenv import load_dotenv
+import requests
 
-#Get the path to the output of inaturalist
-CSV_PATH = '/home/dbgi/output/csv/jbn/audrey_le_cabec.csv'
-
-#create SQL query 
-sql = f'''
-/* Create a temporary table with the columns found in the csv file 
-and copy the values from the csv file
-*/
-
-CREATE TEMP TABLE tmp_x (
-        sample_name TEXT,
-        sample_id TEXT,
-        picture_panel TEXT,
-        picture_general TEXT,
-        picture_detail TEXT,
-        picture_cut TEXT,
-        picture_panel_label TEXT,
-        x_coord NUMERIC,
-        y_coord NUMERIC,
-        geometry TEXT,
-        longitude NUMERIC,
-        latitude NUMERIC
-); -- but see below
-
-
-COPY tmp_x FROM '{CSV_PATH}' delimiter ',' csv header;
-
---Insert the values from the temporary table in the pyinat table where the ids do not already exist
-
-INSERT INTO qfield
-SELECT * FROM tmp_x
-WHERE sample_id NOT IN (SELECT sample_id FROM qfield);
-
---Update the values of the columns where the ids already exist and match
-
-UPDATE pyinat
-SET     quality_grade = tmp_x.quality_grade ,
-        sample_name = tmp_x.sample_name ,
-        picture_panel = tmp_x.picture_panel ,
-        picture_general = tmp_x.picture_general ,
-        picture_detail = tmp_x.picture_detail ,
-        picture_cut = tmp_x.picture_cut ,
-        picture_panel_label = tmp_x.picture_panel_label ,
-        x_coord = tmp_x.x_coord ,
-        y_coord = tmp_x.y_coord ,
-        geometry = tmp_x.geometry ,
-        longitude = tmp_x.longitude ,
-        latitude = tmp_x.latitude
-FROM tmp_x  
-WHERE qfield.sample_id = tmp_x.sample_id 
-        ;
-
---Drop the temporary table
-DROP TABLE tmp_x; -- else it is dropped at end of session automatically
-
-'''
-
-# import env variable
 load_dotenv()
 
-usr=os.getenv('DB_USR')
-pwd=os.getenv('DIRECTUS_PWD')
-vpn_user=os.getenv('VPN_USER')
-vpn_pwd=os.getenv('VPN_PWD')
-vpn_server=os.getenv('VPN_SERVER')
+# import env variable
+directus_email = os.getenv('directus_email')
+directus_password = os.getenv('directus_password')
 
+# Define the Directus table url
+base_url = 'http://directus.dbgi.org'
 
-#Add the VPN connection
-from vpnc import VPNC
+# Define the login endpoint URL
+login_url = base_url + '/auth/login'
 
-vpn = VPNC(config={
-    'Xauth_username': vpn_user,
-    'Xauth_password': vpn_pwd,
-    'IPSec_gateway': vpn_server,
-    'IPSec_ID': 'UNIFR',
-    'IPSec_secret':
-    'IKE_Authmode': 'psk',
-})
+# Create a session object for making requests
+session = requests.Session()
 
-# establish connections
-with vpn.vpn():
-        conn1 = psycopg2.connect(
-	        database="directus_dbgi",
-                user=usr,
-                password=pwd,
-                host='127.0.0.1',
-                port= '5432'
-                )
+# Send a POST request to the login endpoint
+response = session.post(login_url, json={'email': directus_email, 'password': directus_password})
+data = response.json()['data']
+directus_access_token = data['access_token']
+collection_url = base_url + '/items/Qfield_Data/'
+session.headers.update({'Authorization': f'Bearer {directus_access_token}'})
 
-        #conn1.autocommit = True
+#Add headers
+headers = {
+                'Content-Type': 'application/json'
+    }
 
-        # execute query
-        cursor = conn1.cursor()
-        cursor.execute(sql)
+out_csv_path = os.getenv('out_csv_path')
 
-        # commit and close connection
-        conn1.commit()
-        conn1.close()
+# Iterate over all CSV files in the input folder and its subdirectories
+for root, dirs, files in os.walk(out_csv_path):
+    for filename in files:
+        if filename.endswith('.csv'):
+                print(out_csv_path)
+                print(filename)
+                #data = {'field_sample_id': id,
+                        #'sample_name': first_identification,
+                        #'pictures': other_identifications,
+                        #'x_coord': comments_string,
+                        #'y_coord': observation_url,
+                        #'ipen': ipen}
+                #print(data)
+                #response = session.post(url=collection_url, headers=headers, json=data)
+                #print(response.status_code)
+                #if response.status_code != 200:
+                        #collection_url_patch = base_url + '/items/QField_Data/' + id
+                        #response = session.patch(url=collection_url_patch, headers=headers, json=data)
+                        #print(response.status_code)
+
