e1a2232f4d30b17aba0f425d97808c67e5cdc659
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-03-14 13:44:54 +0100

    some tests

--- DBGI_scripts_runner.py
+++ DBGI_scripts_runner.py
@@ -15,18 +15,18 @@ subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_CSV_preparator.py']
 subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_directus_import.py']).check_returncode()
 
 # Run script 5
-subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_renamer.py']).check_returncode()
+#subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_renamer.py']).check_returncode()
 
 # Run script 6
-subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_size_control.py']).check_returncode()
+#subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_size_control.py']).check_returncode()
 
 # Run script 7
-subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_metadata_editor.py']).check_returncode()
+#subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_pictures_metadata_editor.py']).check_returncode()
 
 # Run script 8
-subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_inat_formatter.py']).check_returncode()
+#subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_inat_formatter.py']).check_returncode()
 
 # Run script 9
-subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_Nextcloud_import.py']).check_returncode()
+#subprocess.run(['python3', '/softwares/Pictures_manager/DBGI_Nextcloud_import.py']).check_returncode()
 
 
