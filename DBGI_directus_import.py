4f84e749ff5f209d0b0cb7600f79b475eb553148
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-04-19 16:33:32 +0200

    some changes

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -40,7 +40,7 @@ for root, dirs, files in os.walk(out_csv_path):
                 df = pd.read_csv(constructed_path)
                 treated_df = df.loc[:, ['sample_id', 'sample_name', 'latitude', 'longitude', 'ipen']]
                 treated_df.rename(columns={'sample_id':'field_sample_id'}, inplace=True)
-                data_cleaned = {k: v if pd.notna(v) else None for k, v in treated_df.iterrows()}
+                data_cleaned = {k: v if treated_df.notna(v) else None for k, v in treated_df.iterrows()}
                 for index, row in data_cleaned.iterrows():
                      if row["field_sample_id"] != None:
                         data = {'field_sample_id': row["field_sample_id"],
