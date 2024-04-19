270cb031046901d5d37ececf73b56325f8321caa
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-04-19 16:36:24 +0200

    test replace NA by ''

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -40,8 +40,8 @@ for root, dirs, files in os.walk(out_csv_path):
                 df = pd.read_csv(constructed_path)
                 treated_df = df.loc[:, ['sample_id', 'sample_name', 'latitude', 'longitude', 'ipen']]
                 treated_df.rename(columns={'sample_id':'field_sample_id'}, inplace=True)
-                data_cleaned = {k: v if treated_df.notna(v) else None for k, v in treated_df.iterrows()}
-                for index, row in data_cleaned.iterrows():
+                treated_df.fillna('', inplace=True)
+                for index, row in treated_df.iterrows():
                      if row["field_sample_id"] != None:
                         data = {'field_sample_id': row["field_sample_id"],
                                 'sample_name': row["sample_name"],
