0191095a5e319df616c190612cca2a9fd4de405d
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-04-23 07:23:01 +0200

    added new fields

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -38,16 +38,27 @@ for root, dirs, files in os.walk(out_csv_path):
         if filename.endswith('.csv') and filename != "SBL_20004_2022_EPSG:4326.csv":
                 constructed_path = root + "/" + filename
                 df = pd.read_csv(constructed_path)
-                treated_df = df.loc[:, ['sample_id', 'sample_name', 'latitude', 'longitude', 'ipen']]
-                treated_df.rename(columns={'sample_id':'field_sample_id'}, inplace=True)
+                treated_df = df.loc[:, ['sample_id', 'sample_name', 'latitude', 'longitude', 'ipen', 'no_name_on_list', 'name_proposition', 'herbivory_(percent)', 'comment_eco', 'soil_type', 'weather', 'temperature_(°C)', 'comment_env']]
                 treated_df.fillna('', inplace=True)
                 print(treated_df)
                 for index, row in treated_df.iterrows():
-                     if row["field_sample_id"] != None:
+                     if row["field_sample_id"] != '':
+                        if row["no_name_on_list"] == 0:
+                              treated_df.rename(columns={'sample_id':'field_sample_id'}, inplace=True)
+                        elif row["no_name_on_list"] == 1:
+                              treated_df.rename(columns={'name_proposition':'field_sample_id'}, inplace=True)
                         data = {'field_sample_id': row["field_sample_id"],
                                 'sample_name': row["sample_name"],
                                 'latitude': row["latitude"],
-                                'longitude': row["longitude"]}
+                                'longitude': row["longitude"],
+                                'ipen': row["ipen"],
+                                'herbivory_(percent)': row["herbivory_(percent)"],
+                                'comment_eco': row["comment_eco"],
+                                'soil_type': row["soil_type"],
+                                'weather': row["weather"],
+                                'temperature_(°C)': row["temperature_(°C)"],
+                                'comment_env': row["comment_env"]}
+                        
                         response = session.post(url=collection_url, headers=headers, json=data)
                         print(response.status_code)
                         if response.status_code != 200:
@@ -55,7 +66,7 @@ for root, dirs, files in os.walk(out_csv_path):
                                 response = session.patch(url=collection_url_patch, headers=headers, json=data)
                                 print(response.status_code)
                         else:
-                                print("grec?!")
+                                print("error loading data")
                      else:
-                           print("grec!!")   
+                           print("no field sample id")   
 
