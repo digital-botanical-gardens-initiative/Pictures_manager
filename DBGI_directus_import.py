42c8a5bda6d46243d45fa8eaea57a54ddf232d1a
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-04-23 08:35:21 +0200

    test

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -56,11 +56,11 @@ for root, dirs, files in os.walk(out_csv_path):
                                 'latitude': row["latitude"],
                                 'longitude': row["longitude"],
                                 'ipen': row["ipen"],
-                                'herbivory_percent': row["herbivory_(percent)"],
+                                #'herbivory_percent': row["herbivory_(percent)"],
                                 'comment_eco': row["comment_eco"],
                                 'soil_type': row["soil_type"],
                                 'weather': row["weather"],
-                                #'temperature_celsius': row["temperature_(°C)"],
+                                'temperature_celsius': row["temperature_(°C)"],
                                 'comment_env': row["comment_env"]}
                         
                         response = session.post(url=collection_url, headers=headers, json=data)
