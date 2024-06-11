8d12bd12b4888a5351ffe8383098cd9ee6c73780
edouardbruelhart <edouard.bruelhart@unifr.ch>
2024-06-11 16:29:33 +0200

    some even more updates

--- DBGI_directus_import.py
+++ DBGI_directus_import.py
@@ -38,11 +38,11 @@ for root, dirs, files in os.walk(out_csv_path):
       for filename in files:
             # Ignore old layer without sample_id
             if filename.endswith('.csv') and filename != "SBL_20004_2022_EPSG:4326.csv":
+                  print(filename)
                   # Read each df
                   constructed_path = root + "/" + filename
                   df = pd.read_csv(constructed_path)
                   if filename.endswith("_obs_EPSG:4326.csv"):
-                        print(filename)
                         treated_df = df.loc[:, ['collector_fullname', 'inat_upload', 'taxon_name', 'latitude', 'longitude', 'ipen', 'no_name_on_list', 'name_proposition', 'herbivory_(percent)', 'comment_eco', 'soil_type', 'weather', 'temperature_(°C)', 'comment_env', 'collector_orcid', 'collector_inat', 'date', 'is_wild']]
                         # Replace NAs by nothing, otherwise directus raises an error
                         treated_df.fillna('', inplace=True)
@@ -61,6 +61,8 @@ for root, dirs, files in os.walk(out_csv_path):
                                     inat_upload = True
                               else:
                                           inat_upload = False
+                              print(is_wild)
+                              print(inat_upload)
                               # Create json for data import
                               data = {'latitude': str(row["latitude"]),
                                     'field_sample_name': field_sample_name,
@@ -96,13 +98,13 @@ for root, dirs, files in os.walk(out_csv_path):
                                           #print(response.json())
                   else:
                         # Homogeneize data for directus import
+                        print(filename)
                         treated_df = df.loc[:, ['collector_fullname', 'observation_subject', 'inat_upload', 'sample_id', 'taxon_name', 'latitude', 'longitude', 'ipen', 'no_name_on_list', 'name_proposition', 'herbivory_(percent)', 'comment_eco', 'soil_type', 'weather', 'temperature_(°C)', 'comment_env', 'date', 'collector_orcid', 'collector_inat', 'is_wild']]
                         treated_df.rename(columns={'sample_id':'field_sample_id'}, inplace=True)
                         # Remove possible whitespaces
                         treated_df["field_sample_id"] = treated_df["field_sample_id"].str.strip()
                         # Replace NAs by nothing, otherwise directus raises an error
                         treated_df.fillna('', inplace=True)
-                        print(treated_df)
                         # Send each row individually to directus
                         for index, row in treated_df.iterrows():
                               # Check that sample_id is not null
@@ -120,6 +122,8 @@ for root, dirs, files in os.walk(out_csv_path):
                                           inat_upload = True
                                     else:
                                           inat_upload = False
+                                    print(is_wild)
+                                    print(inat_upload)
                                     # Create json for data import
                                     data = {'field_sample_id_pk': row["field_sample_id"],
                                           'field_sample_id_fk': row["field_sample_id"],
